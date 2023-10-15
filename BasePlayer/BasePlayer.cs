549fcd66faba175dcabfd14bcf338388fe9bb6a0
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-15 21:18:38 +0200

    First commit of the movement engine Added base project Added test level scene Added placeholder assets
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

---
+++ BasePlayer/BasePlayer.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class BasePlayer : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+		Console.WriteLine(direction);
+
+		if (direction.X != 0)
+		{
+
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		if (direction.Y != 0)
+		{
+			velocity.Y = direction.Y * Speed;
+		}
+		else
+		{
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		Velocity = velocity;
+
+		MoveAndSlide();
+	}
+}
