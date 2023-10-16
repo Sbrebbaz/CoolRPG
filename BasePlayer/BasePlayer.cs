16bf63c0c2db22cb67aeae3567f5dbd5815b69aa
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-16 21:54:24 +0200

    Added WASD support Added Running mechanic Small movement refactor Added base animation management Updated game to scale to screen
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

--- BasePlayer/BasePlayer.cs
+++ BasePlayer/BasePlayer.cs
@@ -4,38 +4,62 @@ using System.Diagnostics;
 
 public partial class BasePlayer : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float BaseSpeed = 300.0f;
+	public const float RunningMultiplier = 1.5f;
 	public const float JumpVelocity = -400.0f;
+
+	public AnimatedSprite2D AnimatedSprite2D { get; set; }
+
+	public override void _Ready()
+	{
+		AnimatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		base._Ready();
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		float movementSpeed = BaseSpeed;
+		AnimatedSprite2D.SpeedScale = 1f;
 
+		if (Input.IsActionPressed("ui_run"))
+		{
+			movementSpeed *= RunningMultiplier;
+			AnimatedSprite2D.SpeedScale *= RunningMultiplier;
+		}
+
+		ManageMovementAnimation(direction);
 		Console.WriteLine(direction);
 
-		if (direction.X != 0)
+		if (direction != Vector2.Zero)
 		{
-
-			velocity.X = direction.X * Speed;
+			velocity.X = direction.X * movementSpeed;	
+			velocity.Y = direction.Y * movementSpeed;
+			Velocity = velocity;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			Velocity = direction;
 		}
-
-		if (direction.Y != 0)
-		{
-			velocity.Y = direction.Y * Speed;
-		}
-		else
-		{
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-		}
-
-		Velocity = velocity;
 
 		MoveAndSlide();
 	}
+
+	private void ManageMovementAnimation(Vector2 direction)
+	{
+		if (direction.X != 0)
+		{
+			AnimatedSprite2D.Play((direction.X > 0) ? "walk_right" : "walk_left");
+		}
+		else if (direction.Y != 0)
+		{
+			AnimatedSprite2D.Play((direction.Y > 0) ? "walk_down" : "walk_up");
+		}
+		else
+		{
+			AnimatedSprite2D.Play("idle");
+		}
+	}
+
 }
