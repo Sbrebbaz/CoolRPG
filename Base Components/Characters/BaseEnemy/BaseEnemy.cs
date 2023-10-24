34bdf9236776c2eb89acc4181ef9677d791c6c93
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-24 20:26:12 +0200

    Updated enemy base AI to follow player when close and roam when the player is far away Created signal for player detect and player hit for enemy Renamed player scene components
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

--- Base Components/Characters/BaseEnemy/BaseEnemy.cs
+++ Base Components/Characters/BaseEnemy/BaseEnemy.cs
@@ -4,10 +4,14 @@ using System.Diagnostics;
 
 public partial class BaseEnemy : CharacterBody2D
 {
-	[Export] public float PlayerDetectionArea = 200.0f;
-	[Export] public float RoamingArea = 100.0f;
-	[Export] public float Speed = 100.0f;
+	[Export] public float PlayerDetectionArea = 200f;
+	[Export] public float PlayerHitArea = 32f;
+	[Export] public float RoamingArea = 100f;
+	[Export] public float Speed = 100f;
 	[Export] public SpriteFrames enemySprite { get; set; }
+
+	[Signal] public delegate void PlayerHitDetectionEventHandler();
+	[Signal] public delegate void PlayerCloseDetectionEventHandler();
 
 	public BasePlayer PlayerDetected;
 	public Vector2 StartingPosition;
@@ -28,40 +32,49 @@ public partial class BaseEnemy : CharacterBody2D
 		_AnimatedSprite2D.Play("idle");
 		Velocity = Vector2.Left * Speed;
 
-
-		((CircleShape2D)GetNode<Area2D>("Area2D").GetNode<CollisionShape2D>("CollisionShape2D").Shape).Radius = PlayerDetectionArea;
+		((CircleShape2D)GetNode<Area2D>("PlayerDetection").GetNode<CollisionShape2D>("PlayerDetectionArea").Shape).Radius = PlayerDetectionArea;
+		((CircleShape2D)GetNode<Area2D>("HitDetection").GetNode<CollisionShape2D>("HitDetectionArea").Shape).Radius = PlayerHitArea;
 
 		base._Ready();
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 direction = new Vector2();
+		//Retrieve current direction
+		Vector2 direction = Velocity / Speed;
 
+		//Player detected
+		//Move towards player
 		if (PlayerDetected != null)
 		{
 			direction = (PlayerDetected.Position - Position).Normalized();
 		}
+		//Player not detected
+		//Roam around
 		else
 		{
-			if (StartingPosition == Position
+			if (Position.DistanceTo(StartingPosition) < 1
 				|| Position.DistanceTo(StartingPosition) > RoamingArea
 				|| GetSlideCollisionCount() > 0)
 			{
 				Vector2 newDirection = new Vector2(_RNG.RandfRange(0, RoamingArea) - RoamingArea / 2, _RNG.RandfRange(0, RoamingArea) - RoamingArea / 2);
 
-				direction = (Position - StartingPosition + newDirection).Normalized();
+				direction = (StartingPosition - Position + newDirection).Normalized();
 			}
 		}
+
+		//Manage animations
 		ManageMovementAnimation();
 
+		//New direction and move management
 		Velocity = direction * Speed;
-
 		MoveAndSlide();
-
 		base._PhysicsProcess(delta);
 	}
 
+	/// <summary>
+	/// Method that manages the base movement animation
+	/// </summary>
 	private void ManageMovementAnimation()
 	{
 		if (Velocity.Abs().X > Velocity.Abs().Y)
@@ -78,19 +91,40 @@ public partial class BaseEnemy : CharacterBody2D
 		}
 	}
 
+	/// <summary>
+	/// Event handler for player detection area enter
+	/// Player enter the Area and assign local variable
+	/// </summary>
+	/// <param name="body">Body that entered the area</param>
 	private void _on_player_detection_body_entered(Node2D body)
 	{
 		if (body is BasePlayer)
 		{
 			PlayerDetected = (BasePlayer)body;
+			EmitSignal(SignalName.PlayerCloseDetection);
 		}
 	}
 
+	/// <summary>
+	/// Event handler for player detection area exit
+	/// Player exit the Area and reset local variable
+	/// </summary>
+	/// <param name="body">Body that exited the area</param>
 	private void _on_player_detection_body_exited(Node2D body)
 	{
 		if (body is BasePlayer)
 		{
 			PlayerDetected = null;
 		}
+	}
+
+	/// <summary>
+	/// Event handler for player detection hit area enter
+	/// Player enter the hit Area and raises the signal
+	/// </summary>
+	/// <param name="body">Body that entered the area</param>
+	private void _on_hit_detection_body_entered(Node2D body)
+	{
+		EmitSignal(SignalName.PlayerHitDetection);
 	}
 }
