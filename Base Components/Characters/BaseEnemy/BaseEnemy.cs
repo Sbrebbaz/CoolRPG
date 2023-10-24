0cd0f24a8b23b9ee46b066e1da5c616ed2745956
Davide Nembrini <davide.nembrini00@gmail.com>
2023-10-24 18:21:26 +0200

    Updated base Enemy scene to have player detection and Export base fields Added enemy variation resources for sprites
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

--- Base Components/Characters/BaseEnemy/BaseEnemy.cs
+++ Base Components/Characters/BaseEnemy/BaseEnemy.cs
@@ -4,35 +4,59 @@ using System.Diagnostics;
 
 public partial class BaseEnemy : CharacterBody2D
 {
-	public const float speed = 100.0f;
-	public AnimatedSprite2D AnimatedSprite2D {get; set;}
-	private Vector2 StartPos;
+	[Export] public float PlayerDetectionArea = 200.0f;
+	[Export] public float RoamingArea = 100.0f;
+	[Export] public float Speed = 100.0f;
+	[Export] public SpriteFrames enemySprite { get; set; }
+
+	public BasePlayer PlayerDetected;
+	public Vector2 StartingPosition;
+
+	private AnimatedSprite2D _AnimatedSprite2D;
+	private RandomNumberGenerator _RNG;
+
 	public override void _Ready()
 	{
-		StartPos = Position;
-		Velocity = Vector2.Left * speed;
-		GD.Print(StartPos);
-		AnimatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_RNG = new RandomNumberGenerator();
+
+		StartingPosition = Position;
+
+		Debug.WriteLine(StartingPosition);
+
+		_AnimatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_AnimatedSprite2D.SpriteFrames = enemySprite;
+		_AnimatedSprite2D.Play("idle");
+		Velocity = Vector2.Left * Speed;
+
+
+		((CircleShape2D)GetNode<Area2D>("Area2D").GetNode<CollisionShape2D>("CollisionShape2D").Shape).Radius = PlayerDetectionArea;
+
 		base._Ready();
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-		RandomNumberGenerator rand = new RandomNumberGenerator();
-		if(Position.DistanceTo(StartPos) > 100 || GetSlideCollisionCount() != 0)
+		Vector2 direction = new Vector2();
+
+		if (PlayerDetected != null)
 		{
-			velocity = (StartPos - Position + new Vector2(rand.RandfRange(-50, 50), rand.RandfRange(-50, 50))).Normalized() * speed;
+			direction = (PlayerDetected.Position - Position).Normalized();
+		}
+		else
+		{
+			if (StartingPosition == Position
+				|| Position.DistanceTo(StartingPosition) > RoamingArea
+				|| GetSlideCollisionCount() > 0)
+			{
+				Vector2 newDirection = new Vector2(_RNG.RandfRange(0, RoamingArea) - RoamingArea / 2, _RNG.RandfRange(0, RoamingArea) - RoamingArea / 2);
+
+				direction = (Position - StartingPosition + newDirection).Normalized();
+			}
 		}
 		ManageMovementAnimation();
 
-		Velocity = velocity;
+		Velocity = direction * Speed;
 
-		// if(GetSlideCollisionCount() != 0)
-		// {
-		// 	GD.Print("I HIT SOMETHING");
-		// 	Velocity = Vector2.Right;
-		// }
 		MoveAndSlide();
 
 		base._PhysicsProcess(delta);
@@ -40,17 +64,33 @@ public partial class BaseEnemy : CharacterBody2D
 
 	private void ManageMovementAnimation()
 	{
-		if(Velocity.Abs().X > Velocity.Abs().Y)
+		if (Velocity.Abs().X > Velocity.Abs().Y)
 		{
-			AnimatedSprite2D.Play(Velocity.X > 0? "walk_right" : "walk_left");
+			_AnimatedSprite2D.Play(Velocity.X > 0 ? "walk_right" : "walk_left");
 		}
-		else if(Velocity.Abs().X < Velocity.Abs().Y)
+		else if (Velocity.Abs().X < Velocity.Abs().Y)
 		{
-			AnimatedSprite2D.Play(Velocity.Y > 0? "walk_down" : "walk_up");
+			_AnimatedSprite2D.Play(Velocity.Y > 0 ? "walk_down" : "walk_up");
 		}
 		else
 		{
-			AnimatedSprite2D.Play("idle");
+			_AnimatedSprite2D.Play("idle");
+		}
+	}
+
+	private void _on_player_detection_body_entered(Node2D body)
+	{
+		if (body is BasePlayer)
+		{
+			PlayerDetected = (BasePlayer)body;
+		}
+	}
+
+	private void _on_player_detection_body_exited(Node2D body)
+	{
+		if (body is BasePlayer)
+		{
+			PlayerDetected = null;
 		}
 	}
 }
