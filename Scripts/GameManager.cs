fdca4cca5c0a83b6f78923bd2b67c32d79762efa
Davide Nembrini <davide.nembrini00@gmail.com>
2024-02-07 21:58:40 +0100

    Added empty music manager
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -3,29 +3,37 @@ using System;
 using System.Collections.Generic;
 using static Enumerators;
 
-public partial class GameManager : Node, IDialogManager, ISceneManager, IPlayerManager, ISoundManager
+public partial class GameManager 
+	: Node, 
+	IDialogManager, 
+	ISceneManager, 
+	IPlayerManager, 
+	ISoundManager,
+	IMusicManager
 {
-	private SceneManager _sceneNavigator;
+	private SceneManager _sceneManager;
 	private DialogManager _dialogManager;
 	private PlayerManager _playerManager;
 	private SoundManager _soundManager;
+	private MusicManager _musicManager;
 
 	public override void _Ready()
 	{
-		_sceneNavigator = GetNode<SceneManager>("/root/SceneNavigator");
+		_sceneManager = GetNode<SceneManager>("/root/SceneManager");
 		_dialogManager = GetNode<DialogManager>("/root/DialogManager");
 		_playerManager = GetNode<PlayerManager>("/root/PlayerManager");
 		_soundManager = GetNode<SoundManager>("/root/SoundManager");
+		_musicManager = GetNode<MusicManager>("/root/MusicManager");
 	}
 
 	public void NavigateToScene(string scenePath)
 	{
-		_sceneNavigator.NavigateToScene(scenePath);
+		_sceneManager.NavigateToScene(scenePath);
 	}
 
 	public void NavigateToGameState(GameState gameState)
 	{
-		_sceneNavigator.NavigateToGameState(gameState);
+		_sceneManager.NavigateToGameState(gameState);
 	}
 
 	public void ShowDialog(DialogBase dialog)
---
+++ Scripts/Managers/MusicManager.cs
@@ -0,0 +1,6 @@
+﻿using Godot;
+
+public partial class MusicManager : Node, IMusicManager
+{
+
+}
